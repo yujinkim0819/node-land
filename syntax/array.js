c03a40279171aa985611cf83b027b2e1907e08ff
yujinkim0819 <a76363282@gmail.com>
2022-04-13 08:19:57 +0900

    [JS] 배열 #5

---
+++ syntax/array.js
@@ -0,0 +1,9 @@
+//CRUD
+var arr = ['A','B','C','D']; // 방번호는 0부터
+console.log(arr[1]);
+console.log(arr[3]);
+arr[2] = 3;
+console.log(arr);
+console.log(arr.length); // 개수는 1부터
+arr.push('E'); // 뒤에 데이터 추가
+console.log(arr);
