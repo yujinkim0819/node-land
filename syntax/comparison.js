c530af9490fe47a8da275297c18d10feb4844f4a
yujinkim0819 <a76363282@gmail.com>
2022-04-12 16:16:02 +0900

    [JS] 비교 연산자 #4

---
+++ syntax/comparison.js
@@ -0,0 +1,9 @@
+console.log(1+1); // 이항 연산자
+console.log(1==1); // true
+console.log(1==2); // false
+console.log(1>2); // false
+console.log(1<2); // trues
+console.log(1===1); // true
+console.log(1===2); // false
+name = 1; // 대입 연산자
+name == 1; // 비교 연산자
