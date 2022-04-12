905f4b191667b7375a04681d2db3e4f9434db943
yujinkim0819 <a76363282@gmail.com>
2022-04-12 16:36:48 +0900

    [Node] 콘솔에서의 입력값 #4

--- syntax/conditional.js
+++ syntax/conditional.js
@@ -1,6 +1,8 @@
+var args = process.argv;
+console.log(args[2]); // 배열 형식이 들어가있다
 console.log('A');
 console.log('B');
-if(true){
+if(args[2] == '1'){
     console.log('C1');
 } else { // flase
     console.log('C2');
