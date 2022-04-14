62d64582a7a48605c41d2ccd6b1abb691985e0a8
yujinkim0819 <a76363282@gmail.com>
2022-04-14 09:21:51 +0900

    [JS] 함수의 출력 #6

--- syntax/function2.js
+++ syntax/function2.js
@@ -2,6 +2,11 @@ console.log(Math.round(1.6)); // 반올림 : 2
 console.log(Math.round(1.4)); // 반올림 : 1
 
 function sum(first, second) { // 매개변수, parameter
-    console.log(first + second);
+    console.log('a');
+    return first+second;
+    console.log('b'); // return을 만나면 함수 출력과 종료의 의미
+    
 }
-SubmitEvent(2, 4); // 인자, argument
+console.log(SubmitEvent(2, 4)); // 인자, argument
+
+
