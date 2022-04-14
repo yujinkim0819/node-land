35246ed076524247b7a24229df54a923c06482f0
yujinkim0819 <a76363282@gmail.com>
2022-04-14 09:06:06 +0900

    [JS] 함수의 입력 #6

---
+++ syntax/function2.js
@@ -0,0 +1,7 @@
+console.log(Math.round(1.6)); // 반올림 : 2
+console.log(Math.round(1.4)); // 반올림 : 1
+
+function sum(first, second) { // 매개변수, parameter
+    console.log(first + second);
+}
+SubmitEvent(2, 4); // 인자, argument
