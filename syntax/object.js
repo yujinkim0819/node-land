a46addc7b31d6d8554087827ec34fed4e327bad0
yujinkim0819 <a76363282@gmail.com>
2022-04-20 08:19:59 +0900

    [JS] 객체의 반복 #10

--- syntax/object.js
+++ syntax/object.js
@@ -1,9 +1,19 @@
 var members = ['egoing', 'k8805', 'hoya'];
 console.log(members[1]); // k8805
+var i = 0;
+while (i < members.length) {
+    console.log('array loop', members[i]);
+    i = i + 1;
+}
 
+// key : value
 var roles = {
     'programmer':'egoing',
     'designer':'k8805',
     'manager':'hony'
 }
 console.log(roles.designer); // k8805
+
+for (var name in roles) {
+    console.log('object => ', name, 'vaue => ', roles[name]);
+}
